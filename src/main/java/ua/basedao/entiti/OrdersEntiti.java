a7d6721b684d9e67164cd88bf18097fb79ce6639
liny <igorkvjava@gmail.com>
2015-12-10 18:58:28 +0200

    add table orders

---
+++ src/main/java/ua/basedao/entiti/OrdersEntiti.java
@@ -0,0 +1,119 @@
+package ua.basedao.entiti;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by liny on 10.12.15.
+ */
+@Entity
+@Table(name = "orders", schema = "classicmodels")
+public class OrdersEntiti {
+    private int orderNumber;
+    private Serializable orderDate;
+    private Serializable requiredDate;
+    private Serializable shippedDate;
+    private String status;
+    private String comments;
+    private int customerNumber;
+
+    @Id
+    @Column(name = "orderNumber")
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    @Basic
+    @Column(name = "orderDate")
+    public Serializable getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Serializable orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    @Basic
+    @Column(name = "requiredDate")
+    public Serializable getRequiredDate() {
+        return requiredDate;
+    }
+
+    public void setRequiredDate(Serializable requiredDate) {
+        this.requiredDate = requiredDate;
+    }
+
+    @Basic
+    @Column(name = "shippedDate")
+    public Serializable getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(Serializable shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    @Basic
+    @Column(name = "status")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Basic
+    @Column(name = "comments")
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    @Basic
+    @Column(name = "customerNumber")
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrdersEntiti that = (OrdersEntiti) o;
+
+        if (orderNumber != that.orderNumber) return false;
+        if (customerNumber != that.customerNumber) return false;
+        if (orderDate != null ? !orderDate.equals(that.orderDate) : that.orderDate != null) return false;
+        if (requiredDate != null ? !requiredDate.equals(that.requiredDate) : that.requiredDate != null) return false;
+        if (shippedDate != null ? !shippedDate.equals(that.shippedDate) : that.shippedDate != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (comments != null ? !comments.equals(that.comments) : that.comments != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = orderNumber;
+        result = 31 * result + (orderDate != null ? orderDate.hashCode() : 0);
+        result = 31 * result + (requiredDate != null ? requiredDate.hashCode() : 0);
+        result = 31 * result + (shippedDate != null ? shippedDate.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (comments != null ? comments.hashCode() : 0);
+        result = 31 * result + customerNumber;
+        return result;
+    }
+}
